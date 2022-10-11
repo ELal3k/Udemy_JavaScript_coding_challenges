e53cb409b3e2e713c6531669690cfb42420c6503
Alexandros Polymeros <alpolymer@gmail.com>
2022-10-11 20:26:07 +0300

    CC1 done

---
+++ CC1/script.js
@@ -0,0 +1,41 @@
+// DATA1
+
+// const MarkMass1 = 78;
+// const MarkHeight1 = 1.69;
+// const MarkBMI1 = MarkMass1 / MarkHeight1 ** 2;
+
+// const JohnMass1 = 92;
+// const JohnHeight1 = 1.95;
+// const JohnBMI1 = JohnMass1 / JohnHeight1 ** 2;
+
+// let MarkHigherBMI1 = MarkBMI1 > JohnBMI1;
+
+// console.log(MarkBMI1, JohnBMI1, MarkHigherBMI1)
+
+// if (MarkHigherBMI1) {
+//     console.log(`Mark's BMI (${MarkBMI1}) is higher than John's (${JohnBMI1})`)
+// }
+// else {
+//     console.log(`Mark's BMI (${MarkBMI1}) is lower than John's (${JohnBMI1})`)
+// }
+
+// DATA2
+
+const MarkMass2 = 95;
+const MarkHeight2 = 1.88;
+const MarkBMI2 = MarkMass2 / MarkHeight2 ** 2;
+
+const JohnMass2 = 85;
+const JohnHeight2 = 1.76;
+const JohnBMI2 = JohnMass2 / JohnHeight2 ** 2;
+
+let MarkHigherBMI2 = MarkBMI2 > JohnBMI2;
+
+console.log(MarkBMI2, JohnBMI2, MarkHigherBMI2)
+
+if (MarkHigherBMI2) {
+    console.log(`Mark's BMI (${MarkBMI2}) is higher than John's (${JohnBMI2})`)
+}
+else {
+    console.log(`Mark's BMI (${MarkBMI2}) is lower than John's (${JohnBMI2})`)
+};
