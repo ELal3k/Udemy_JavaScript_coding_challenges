c6e92b17b536fb9a6b99ced251c1767d6cb63608
Alexandros Polymeros <alpolymer@gmail.com>
2022-10-18 18:56:39 +0300

    coding challenge 4 with better coding

--- CC4/script.js
+++ CC4/script.js
@@ -5,41 +5,22 @@ const koalasScore1 = Number(prompt("Input Koalas game 1 score:"))
 const koalasScore2 = Number(prompt("Input Koalas game 2 score:"))
 const koalasScore3 = Number(prompt("Input Koalas game 3 score:"))
 
-const calcAverageKoalas = function (score1, score2, score3) {
+const calcAverage = (a, b, c) => (a + b + c) / 3;
 
-    const koalasAvr = (score1 + score2 + score3) / 3;
-    return koalasAvr;
-}
+let scoreDolphins = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
+let scoreKoalas = calcAverage(koalasScore1, koalasScore2, koalasScore3);
 
-console.log(`The average score of Koalas is ${calcAverageKoalas(koalasScore1, koalasScore2, koalasScore3)}`);
+console.log(`The average score of Dolphins team is ${scoreDolphins} points!`);
+console.log(`The average score of Koalas team is ${scoreKoalas} points!`);
 
-const calcAverageDolphins = function (score1, score2, score3) {
-
-    const dolphinsAvr = (score1 + score2 + score3) / 3;
-    return dolphinsAvr;
-}
-
-console.log(`The average score of Dolphins is ${calcAverageDolphins(dolphinsScore1, dolphinsScore2, dolphinsScore3)}`);
-
-const checkWinner = function (koalasAvr, dolphinsAvr) {
-    let winner;
-
-    if (koalasAvr >= 2 * dolphinsAvr) {
-        winner = "Koalas win!";
-
+function checkWinner(dolphinsAvr, koalasAvr) {
+    if (dolphinsAvr >= 2 * koalasAvr) {
+        console.log(`Dolphins win! ${dolphinsAvr} vs ${koalasAvr}`)
     }
-    else if (dolphinsAvr >= 2 * koalasAvr) {
-        winner = "Dolphins win!";
-
+    else if (koalasAvr >= 2 * dolphinsAvr) {
+        console.log(`Koalas win! ${koalasAvr} vs ${dolphinsAvr}`)
     }
+    else { console.log("No one wins") }
+};
 
-    else { winner = "No one wins..." }
-
-    return winner;
-}
-
-console.log(
-    checkWinner(
-        calcAverageKoalas(koalasScore1, koalasScore2, koalasScore3), calcAverageDolphins(dolphinsScore1, dolphinsScore2, dolphinsScore3)
-    )
-);
+checkWinner(scoreDolphins, scoreKoalas);
